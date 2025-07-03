028d94782ad2de684a936cf58064b9fdc695994f
Spitball1234 <liam.jbm2020@gmail.com>
2025-07-03 15:58:58 -0400

    Cleaned code some more. Not that great quality, I messed up the codebase a little and had to fix from memory

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import numpy as np
 
-def generate_points(maximum_points, number_of_clusters, screen_x=4096, screen_y=4096):
+def generate_points(number_of_clusters, screen_x=4096, screen_y=4096):
   all_points = []
   just_clusters = []
-  maximum_spread = maximum_points / 4
-  maximum_attempts = 1
+  minimum_distance = 30 * 4
 
   for _ in range(number_of_clusters):
     attempts_count = 0
-    while attempts_count < maximum_attempts:
-      new_cluster_candidate = (np.random.randint(maximum_spread, screen_x - maximum_spread), np.random.randint(maximum_spread, screen_y - maximum_spread))
+    while attempts_count < 1:
+      new_cluster_candidate = (np.random.randint(30, screen_x - 30), np.random.randint(30, screen_y - 30))
       for existing_cluster_x, existing_cluster_y in just_clusters:
-          distance_between_clusters = np.sqrt((new_cluster_candidate[0] - existing_cluster_x)**2 + (new_cluster_candidate[1] - existing_cluster_y)**2)
-          if distance_between_clusters < maximum_spread:
-            attempts_count += 1
-            break
+        distance_between_clusters = np.sqrt((new_cluster_candidate[0] - existing_cluster_x)**2 + (new_cluster_candidate[1] - existing_cluster_y)**2)
+        if distance_between_clusters < minimum_distance:
+          attempts_count += 1
+          break
       else:
         just_clusters.append(new_cluster_candidate)
         break
 
   for cluster_center_x, cluster_center_y in just_clusters:
-      num_points_to_generate = np.random.randint(maximum_points)
-      for _ in range(num_points_to_generate):
-          offset_x = np.random.randint(-maximum_spread, maximum_spread)
-          offset_y = np.random.randint(-maximum_spread, maximum_spread)
-          
-          generated_point = (cluster_center_x + offset_x, cluster_center_y + offset_y)
-          all_points.append(generated_point)
+    new_points = []
+    num_points_to_generate = np.random.randint(0,30)
+    for _ in range(num_points_to_generate):
+      offset_x = np.random.randint(-30, 30)
+      offset_y = np.random.randint(-30, 30)
+      
+      generated_point = (cluster_center_x + offset_x, cluster_center_y + offset_y)
+      new_points.append(generated_point)
+    all_points.append(new_points)
   return all_points
