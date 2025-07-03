313ccbb0b3a28373b7b162872422704054f81f32
Spitball1234 <liam.jbm2020@gmail.com>
2025-07-03 16:54:25 -0400

    Whitespace fixes

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ def generate_points(num_clusters, width=4096, height=4096):
     minimum_distance = 30 * 4
 
     for _ in range(num_clusters):
-        attempts_count = 0
         for _ in range(10):
-            new_cluster = (np.random.randint(30, width - 30),
-                                     np.random.randint(30, height - 30))
+            new_cluster = (np.random.randint(30, width - 30), 
+            np.random.randint(30, height - 30))
             for old_cluster_x, old_cluster_y in old_clusters:
                 distance_between_clusters = np.sqrt(
                     (new_cluster[0] - old_cluster_x)**2 +
@@ -25,7 +24,6 @@ def generate_points(num_clusters, width=4096, height=4096):
         new_points = []
         future_points = np.random.randint(0, 30)
         for _ in range(future_points):
-
             generated_point = (cluster_center_x + np.random.randint(-30, 30),
                                cluster_center_y + np.random.randint(-30, 30))
             new_points.append(generated_point)
