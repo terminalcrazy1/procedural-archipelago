c41c5222d723ff93afca999fc667f9f16f9793be
Spitball1234 <liam.jbm2020@gmail.com>
2025-07-03 17:04:03 -0400

    Optimize dependencies (use as little of numpy as possible)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
+from numpy import sqrt
+from numpy.random import randint as ri
 
 def generate_points(num_clusters, width=4096, height=4096):
     old_points = []
@@ -6,10 +7,10 @@ def generate_points(num_clusters, width=4096, height=4096):
 
     for _ in range(num_clusters):
         for _ in range(10):
-            new_cluster = (np.random.randint(30, width - 30), 
-            np.random.randint(30, height - 30))
+            new_cluster = (ri(30, width - 30), 
+            ri(30, height - 30))
             for old_cluster_x, old_cluster_y in old_clusters:
-                distance_between_clusters = np.sqrt(
+                distance_between_clusters = sqrt(
                     (new_cluster[0] - old_cluster_x)**2 +
                     (new_cluster[1] - old_cluster_y)**2
                 )
@@ -21,10 +22,10 @@ def generate_points(num_clusters, width=4096, height=4096):
 
     for cluster_center_x, cluster_center_y in old_clusters:
         new_points = []
-        future_points = np.random.randint(0, 30)
+        future_points = ri(0, 30)
         for _ in range(future_points):
-            generated_point = (cluster_center_x + np.random.randint(-30, 30),
-                               cluster_center_y + np.random.randint(-30, 30))
+            generated_point = (cluster_center_x + ri(-30, 30),
+                               cluster_center_y + ri(-30, 30))
             new_points.append(generated_point)
         old_points.append(new_points)
     return old_points
